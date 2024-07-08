27a81535cebc6d11eaf0f96e280b708788fd89f9
Charis Lam <26616127+charislam@users.noreply.github.com>
2024-07-08 19:47:52 -0400

    fix: reset password redirect link

--- app/private/reset-password.tsx
+++ app/private/reset-password.tsx
@@ -14,7 +14,11 @@ function ResetPassword({ email }: { email: string }) {
 
     if (email) {
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: "http://127.0.0.1:3000/login/password-update",
+        redirectTo: `${
+          process.env.NEXT_PUBLIC_VERCEL_URL
+            ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+            : "http://127.0.0.1:3000"
+        }/login/password-update`,
       });
       if (error) {
         console.error(error);
